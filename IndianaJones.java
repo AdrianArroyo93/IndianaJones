bf820302f93e2c05796c74bc8398ac24c3708795
Adrian <adrianarroyo93@gmail.com>
2025-02-17 00:00:06 +0100

    IndianaJones

---
+++ IndianaJones.java
@@ -0,0 +1,19 @@
+package IndianaJones;
+
+public class IndianaJones {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int numero = 1;
+		for (int i = 0; i<=100 ; i++) {
+		if (i == numero*7 ) {
+			System.out.println(i);
+		}
+		System.out.println("Indiana Jones ha entrado en la valdosa" + i);
+			
+		}
+		
+	}
+
+}
